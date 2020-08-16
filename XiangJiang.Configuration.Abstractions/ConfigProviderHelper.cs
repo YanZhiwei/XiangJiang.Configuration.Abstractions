886fcd8d75c95132a4089f552dac555a0caa98c3
YanZhiwei <Yan.Zhiwei@hotmail.com>
2020-08-16 23:21:52 +0800

    add ConfigContext

---
+++ XiangJiang.Configuration.Abstractions/ConfigProviderHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace XiangJiang.Configuration.Abstractions
+{
+    public static class ConfigProviderHelper
+    {
+        public static string CreateClusteredIndex<T>(string index) where T : class, new()
+        {
+            var fileName = typeof(T).Name;
+            return string.IsNullOrEmpty(index) ? fileName : $"{fileName}_{index}";
+        }
+    }
+}
